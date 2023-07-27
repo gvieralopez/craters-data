452ac3e6a8e5a86672cb2ac4104c2747d47ebe30
Gustavo Viera López <gvieralopez@gmail.com>
2023-07-27 13:45:28 +0200

    Add example of how to load the data

---
+++ example.py
@@ -0,0 +1,8 @@
+from craterslab.sensors import DepthMap
+from craterslab.visuals import plot_2D, plot_3D
+
+depth_map = DepthMap.load('examples/data/fluidized_1.npz')
+depth_map.auto_crop()
+
+plot_3D(depth_map, preview_scale=(1, 1, 4))
+plot_2D(depth_map, block=True)
